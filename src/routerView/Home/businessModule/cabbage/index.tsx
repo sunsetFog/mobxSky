1e463186af972dcaf1b2888a1eddb254d96a3cf6
ivory <jsivory@itcom666.com>
2023-12-04 15:37:22 +0800

    2023-12-4 15:38

--- src/routerView/Home/businessModule/cabbage/index.tsx
+++ src/routerView/Home/businessModule/cabbage/index.tsx
@@ -3,12 +3,23 @@ import { observer } from 'mobx-react';
 // component
 import { compose } from '@/utils/redux';
 import PopupUnit from '@/@energy/@pcDesign/components/popupUnit';
+import { convertCanvasToImage } from '@/@energy/@utils/html2canvas';
 
 function cabbageUnit(props: any) {
     useEffect(() => {});
     return (
         <section>
             <PopupUnit>自定义弹窗</PopupUnit>
+            <br />
+            <br />
+            <button
+                id='toImg'
+                onClick={() => {
+                    convertCanvasToImage(document.getElementById('toImg'));
+                }}
+            >
+                元素转成图片
+            </button>
         </section>
     );
 }
