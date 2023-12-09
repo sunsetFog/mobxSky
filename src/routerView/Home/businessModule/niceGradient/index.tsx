ead9a1c48a66337fd86e3c56c2dda759f4d1950a
ivory <jsivory@itcom666.com>
2023-12-09 16:28:23 +0800

    2023-12-9 16:28

--- src/routerView/Home/businessModule/niceGradient/index.tsx
+++ src/routerView/Home/businessModule/niceGradient/index.tsx
@@ -4,12 +4,45 @@ import { observer } from 'mobx-react';
 import { compose } from '@/utils/redux';
 import styles from './index.module.scss';
 import LineTextLine from '@/components/lineTextLine/index';
+/*
 
+线性渐变
+参数1：角度，顺时针旋转
+    0deg或to top  从下到上
+    45deg 从左下角到右上角
+    90deg或to right  从左到右
+    180deg或to bottom  从上到下---默认
+    270deg或to left  从右到左
+参数2：颜色 百分比
+参数3：颜色 百分比
+
+多块
+background-image: linear-gradient(),linear-gradient(),linear-gradient();
+
+径向渐变--有兼容性
+background-image: radial-gradient(shape size at position, color 百分比, ..., );
+    shape确定圆的类型:
+        ellipse (默认): 指定椭圆形的径向渐变
+        circle ：指定圆形的径向渐变
+    size渐变的大小
+        farthest-corner (默认) : 指定径向渐变的半径长度为从圆心到离圆心最远的角
+        closest-side ：指定径向渐变的半径长度为从圆心到离圆心最近的边
+        closest-corner ： 指定径向渐变的半径长度为从圆心到离圆心最近的角
+        farthest-side ：指定径向渐变的半径长度为从圆心到离圆心最远的边
+    position渐变的位置
+        center（默认）：设置中间为径向渐变圆心的纵坐标值。
+        top：设置顶部为径向渐变圆心的纵坐标值。
+        bottom：设置底部为径向渐变圆心的纵坐标值。
+        或值60% 55%
+background-image: radial-gradient(circle farthest-side at 60% 55%, red 10%, yellow 60%, green 100%);
+
+*/
 function niceGradient(props: any) {
     useEffect(() => {});
     return (
         <section className={styles.niceGradient}>
             <LineTextLine>好看的渐变</LineTextLine>
+            <div className={styles.fishBox}></div>
         </section>
     );
 }
--- src/routerView/Home/businessModule/niceShadow/index.tsx
+++ src/routerView/Home/businessModule/niceShadow/index.tsx
@@ -4,12 +4,24 @@ import { observer } from 'mobx-react';
 import { compose } from '@/utils/redux';
 import styles from './index.module.scss';
 import LineTextLine from '@/components/lineTextLine/index';
-
+/*
+box-shadow: inset h-shadow v-shadow blur spread color;
+inset：内部阴影，不写就是外阴影。
+h-shadow：
+外部阴影时：右左边框，水平阴影的偏移量。正值是右边框（向右偏移）或 负值是左边框（向左偏移）。
+内部阴影时：右左边框，水平阴影的偏移量。正值是左边框（向右偏移）或 负值是右边框（向左偏移）。
+v-shadow：
+外部阴影时：下上边框，垂直阴影的偏移量。正值是下边框（向下偏移）或 负值是上边框（向上偏移）。
+内部阴影时：下上边框，垂直阴影的偏移量。正值是上边框（向下偏移）或 负值是下边框（向上偏移）。
+blur：模糊半径。值越大，阴影越模糊
+spread：阴影的扩展距离。正值会使阴影变大，负值会使阴影变小。
+color：颜色。
+*/
 function niceShadow(props: any) {
     useEffect(() => {});
     return (
         <section className={styles.niceShadow}>
-            <LineTextLine>白色背景的阴影</LineTextLine>
+            <LineTextLine>白色背景的外部阴影</LineTextLine>
             <div className={styles.whiteLegend}>
                 <ul>
                     <li>
@@ -20,6 +32,13 @@ function niceShadow(props: any) {
                     </li>
                     <div style={{ clear: 'both' }}></div>
                 </ul>
+            </div>
+            <LineTextLine>输入框的内部阴影</LineTextLine>
+            <div className={styles.birdBox}>
+                <input type='text' placeholder='请输入内容' />
+                <div className={styles.searchBox}>
+                    <img src={require('./img/icon_search.png')} />
+                </div>
             </div>
         </section>
     );
