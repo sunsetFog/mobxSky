56d63bff24f9d1f0ce12673cc18d6a827f73733f
ivory <jsivory@itcom666.com>
2023-12-30 19:15:45 +0800

    2023-12-30 19:15

--- src/routerView/Home/businessModule/beautifulBox/index.tsx
+++ src/routerView/Home/businessModule/beautifulBox/index.tsx
@@ -13,83 +13,91 @@ function beautifulBox(props: any) {
     };
     return (
         <section className={styles.beautifulBox}>
-            <main className={styles.main1}>
-                <header>
-                    {tabArr1.map((item, index) => {
-                        return (
-                            <label
-                                key={index}
-                                className={
-                                    tabKey == index
-                                        ? styles[item.activeBg]
-                                        : styles[item.conventionBg]
-                                }
-                                style={{ left: index * 120, zIndex: tabArr1.length - index }}
-                                onClick={() => {
-                                    tabWay(index);
-                                }}
-                            >
-                                {item.title}
-                            </label>
-                        );
-                    })}
-                </header>
-                <article></article>
-            </main>
-            <main className={styles.main2}>
-                <header>
-                    <ul>
+            <section>
+                <main className={styles.main1}>
+                    <header>
                         {tabArr1.map((item, index) => {
                             return (
-                                <li
+                                <label
                                     key={index}
+                                    className={
+                                        tabKey == index
+                                            ? styles[item.activeBg]
+                                            : styles[item.conventionBg]
+                                    }
+                                    style={{ left: index * 120, zIndex: tabArr1.length - index }}
                                     onClick={() => {
                                         tabWay(index);
                                     }}
                                 >
-                                    {tabKey == index ? (
-                                        <div className={styles.cartoon}>
-                                            <p>{item.title}</p>
-                                            <div>
-                                                <img src={item.activeIcon} />
+                                    {item.title}
+                                </label>
+                            );
+                        })}
+                    </header>
+                    <article></article>
+                </main>
+            </section>
+
+            <section>
+                <main className={styles.main2}>
+                    <header>
+                        <ul>
+                            {tabArr1.map((item, index) => {
+                                return (
+                                    <li
+                                        key={index}
+                                        onClick={() => {
+                                            tabWay(index);
+                                        }}
+                                    >
+                                        {tabKey == index ? (
+                                            <div className={styles.cartoon}>
+                                                <p>{item.title}</p>
+                                                <div>
+                                                    <img src={item.activeIcon} />
+                                                </div>
                                             </div>
-                                        </div>
-                                    ) : null}
-                                    <main>
-                                        <div className={styles.imgBox}>
-                                            <img src={item.icon} />
-                                        </div>
-                                        <p className={styles.title}>{item.title}</p>
-                                    </main>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </header>
-                <article></article>
-            </main>
-            <main className={styles.main3}>
-                <header>
-                    <ul>
-                        {tabArr3.map((item, index) => {
-                            return (
-                                <li
-                                    key={index}
-                                    onClick={() => {
-                                        tabWay(index);
-                                    }}
-                                >
-                                    <main className={tabKey == index ? styles.activeBg : ''}>
-                                        <div></div>
-                                        {item.title}
-                                    </main>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </header>
-                <article></article>
-            </main>
+                                        ) : null}
+                                        <main>
+                                            <div className={styles.imgBox}>
+                                                <img src={item.icon} />
+                                            </div>
+                                            <p className={styles.title}>{item.title}</p>
+                                        </main>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </header>
+                    <article></article>
+                </main>
+            </section>
+
+            <section>
+                <main className={styles.main3}>
+                    <header>
+                        <ul>
+                            {tabArr3.map((item, index) => {
+                                return (
+                                    <li
+                                        key={index}
+                                        onClick={() => {
+                                            tabWay(index);
+                                        }}
+                                    >
+                                        <main className={tabKey == index ? styles.activeBg : ''}>
+                                            <div></div>
+                                            {item.title}
+                                        </main>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </header>
+                    <article></article>
+                </main>
+            </section>
         </section>
     );
 }
